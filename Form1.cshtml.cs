c4e0e817313b8d54c1789d039b07b146e1ce06df
Sid Sachs <87913200+kasilakeconsulting@users.noreply.github.com>
2022-02-14 16:48:26 -0500

    Add files via upload

---
+++ Form1.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWorld.Pages
+{
+    public class FormsModel : PageModel
+    {
+        // These variables may be accessed from the cshtml when prefixed with "Model.".
+        public string requestType { get; set; }
+        public bool isPost { get; set; }
+
+
+        public void OnGet()
+        {
+            requestType = "Get";
+            isPost = false;
+        }
+        public void OnPost()
+        {
+            requestType = "Post";
+            isPost = true;
+        }
+    }
+}
---
+++ Form2.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloWorld
+{
+    public class Form2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
